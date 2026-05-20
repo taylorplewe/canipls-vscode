c0effb6022fa8fb74262e78a17d33c785b4c5966
Taylor Plewe <tplewe@outlook.com>
2026-05-20 10:42:02 -0600

    fetch latest canipls release, unzip it, and use it as the server

--- src/extension.ts
+++ src/extension.ts
@@ -5,11 +5,109 @@ import {
     ServerOptions,
     TransportKind,
 } from 'vscode-languageclient/node';
+import extract from 'extract-zip';
 
 let client: LanguageClient | undefined;
 
+const CANIPLS_REPO = "taylorplewe/canipls";
+const CANIPLS_LATEST_RELEASE_URL = `https://api.github.com/repos/${CANIPLS_REPO}/releases/latest`;
+
+class SemVer {
+    major: number = 0;
+    minor: number = 0;
+    patch: number = 0;
+    constructor(str: string) {
+        const semVerRe = /v?(\d+)\.(\d+)\.(\d+)/;
+        const match = str.match(semVerRe);
+        if (!match) {
+            console.log('no match');
+            return;
+        } else if (match.length !== 4) {
+            console.log('invlaid semver string ' + str);
+        }
+        this.major = parseInt(match[1]);
+        this.minor = parseInt(match[2]);
+        this.patch = parseInt(match[3]);
+    }
+
+    gt(other: SemVer): boolean {
+        if (other.major > this.major) return false;
+        if (other.major < this.major) return true;
+        if (other.minor > this.minor) return false;
+        if (other.minor < this.minor) return true;
+        if (other.patch > this.patch) return false;
+        return true;
+    }
+}
+
 export async function activate(context: vscode.ExtensionContext) {
     console.log('canipls activated');
+    console.log('globalStorageUri:', context.globalStorageUri);
+
+    // TODO: check if there's an installed version of canipls at globalStorage/taylorplewe.canipls/
+
+    let arch: string = process.arch;
+    switch (process.arch) {
+        case "x64":
+            arch = "x86_64";
+            break;
+        case "arm64":
+            arch = "aarch64";
+            break;
+    }
+    let os: string = process.platform;
+    switch (process.platform) {
+        case "win32":
+            os = "windows";
+            break;
+        case "darwin":
+            os = "macos";
+            break;
+    }
+
+    const latestReleaseResponse: any = await fetch(CANIPLS_LATEST_RELEASE_URL).then(res => res.json());
+    console.log('latest release res: ', latestReleaseResponse);
+
+    const installedSemVer = new SemVer("0.0.0");
+    const latestSemVer = new SemVer(latestReleaseResponse.name);
+
+    if (latestSemVer.gt(installedSemVer)) {
+        console.log('newer version available');
+    }
+
+    let latestArchiveUrl: string;
+    let caniplsArchivePath: vscode.Uri = vscode.Uri.parse("");
+    for (const asset of latestReleaseResponse.assets) {
+        if (asset.name.indexOf(`${arch}-${os}`) !== -1) {
+            console.log('asset url:', asset.url);
+            const archiveRes = await fetch(asset.url, { headers: {"Accept": "application/octet-stream"} });
+            console.log('content-length: ', archiveRes.headers.get("content-length"));
+            const archiveData = Buffer.from(await archiveRes.arrayBuffer());
+            caniplsArchivePath = vscode.Uri.joinPath(context.globalStorageUri, asset.name);
+
+            // write archive to directory
+            await vscode.workspace.fs.writeFile(caniplsArchivePath, archiveData);
+
+            console.log('caniplsExePath:', caniplsArchivePath.fsPath);
+            console.log('context.globalStorageUri:', context.globalStorageUri.toString());
+
+            // extract zip
+            await extract(caniplsArchivePath.fsPath, { dir: context.globalStorageUri.fsPath });
+
+            console.log('extracted!');
+
+            // cleanup (delete archive)
+            await vscode.workspace.fs.delete(caniplsArchivePath);
+        }
+    }
+
+
+
+
+
+
+    const caniplsExePath = vscode.Uri.joinPath(context.globalStorageUri, `canipls${process.platform === "win32" ? ".exe" : ""}`);
+    console.log('running exe: ', caniplsExePath.fsPath);
 
     context.subscriptions.push(
         vscode.commands.registerCommand("canipls.restartLanguageServer", async () => {
@@ -17,10 +115,8 @@ export async function activate(context: vscode.ExtensionContext) {
         })
     );
 
-    const exeName = "C:\\Users\\tplew\\webroot\\canipls\\zig-out\\bin\\canipls.exe";
-
     const serverOptions: ServerOptions = {
-        command: exeName,
+        command: caniplsExePath.fsPath,
         transport: TransportKind.stdio,
         options: {
             shell: true,
@@ -55,3 +151,5 @@ export async function deactivate() {
     await client?.dispose();
     client = undefined;
 }
+
+// HELPFUL STUFF I MIGHT NEED
